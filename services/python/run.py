9bde15e17367338f697bec55550488c5ea2e9166
Ryan Northey <ryan@synca.io>
2020-11-25 08:12:57 +0000

    more
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- services/python/run.py
+++ services/python/run.py
@@ -1,2 +1,16 @@
-def foo():
-    return 'baz'
+import os
+
+from aiohttp import web
+import aiohttp_cors
+
+
+app = web.Application()
+
+
+async def root_handler(request):
+    return web.Response(text='Hello, aio world')
+
+
+if __name__ == "__main__":
+    app.router.add_route('*', '/', root_handler)
+    web.run_app(app)
