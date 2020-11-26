010d7bad8ecc821ef6dbfaa23d1328f9d129b6cf
Ryan Northey <ryan@synca.io>
2020-11-26 13:13:32 +0000

    playground/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- playground/control/attribs.py
+++ playground/control/attribs.py
@@ -14,10 +14,11 @@ MAX_NETWORK_CONNECTIONS = 10
 
 MIN_NAME_LENGTH = 2
 MAX_NAME_LENGTH = 32
+MIN_CONFIG_LENGTH = 7
+MAX_CONFIG_LENGTH = 4096
 
-
-RE_NOT_NAME = r'(?!.*(__|\.\.|\-\-)+.*$)'
 RE_NAME = r'[a-z]+[a-z0-9\.\-_]*$'
+RE_NOT_NAME = r'(?!.*(__|\.\.|\-\-)+.*$)'
 RE_UUID = r'[0-9a-f]+$'
 
 
