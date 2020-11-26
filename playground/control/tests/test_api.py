291bee28c2d34af07f89234e8e765c895897fb27
Ryan Northey <ryan@synca.io>
2020-11-26 09:14:43 +0000

    playground/
    Signed-off-by: Ryan Northey <ryan@synca.io>

---
+++ playground/control/tests/test_api.py
@@ -0,0 +1,12 @@
+
+import asyncio
+
+import pytest
+
+import playground.control.api
+
+
+@pytest.mark.asyncio
+async def test_some_asyncio_code():
+    await asyncio.sleep(1)
+    assert playground.control.api
--- playground/control/validators.py
+++ playground/control/validators.py
@@ -1,54 +1,114 @@
 import attr
 
 
+# im really surprised this is not there in lib
 @attr.s(repr=False, slots=True, hash=True)
 class _LengthValidator(object):
     length = attr.ib()
+
+    def _gt(self, inst, attr, value):
+        if not len(value) > int(self.length.strip('>')):
+            raise TypeError(
+                "'{name}' must be longer than {length!r} (got {value!r} that is "
+                "{actual!r}).".format(
+                    name=attr.name,
+                    length=self.length.strip('>'),
+                    actual=value.__class__,
+                    value=value,
+                ),
+                attr,
+                self.length,
+                value)
+
+
+    def _gte(self, inst, attr, value):
+        if not len(value) >= int(self.length.strip('>=')):
+            raise TypeError(
+                "'{name}' must be longer or equal to {length!r} (got {value!r} that is "
+                "{actual!r}).".format(
+                    name=attr.name,
+                    length=self.length.strip('>='),
+                    actual=value.__class__,
+                    value=value,
+                ),
+                attr,
+                self.length,
+                value)
+
+    def _lt(self, inst, attr, value):
+        if not len(value) < int(self.length.strip('<')):
+            raise TypeError(
+                "length of '{name}' must be less than {length!r} (got {value!r} that is "
+                "{actual!r}).".format(
+                    name=attr.name,
+                    length=self.length.strip('<'),
+                    actual=len(value),
+                    value=value,
+                ),
+                attr,
+                self.length,
+                value)
+
+    def _lte(self, inst, attr, value):
+        if not len(value) <= int(self.length.strip('<=')):
+            raise TypeError(
+                "length of '{name}' must be less than or equal to {length!r} (got {value!r} that is "
+                "{actual!r}).".format(
+                    name=attr.name,
+                    length=self.length.strip('<='),
+                    actual=len(value),
+                    value=value,
+                ),
+                attr,
+                self.length,
+                value)
+
+    def _eq(self, inst, attr, value):
+        if not len(value) == int(self.length):
+            raise TypeError(
+                "length of '{name}' must be equal to {length!r} (got {value!r} that is "
+                "{actual!r}).".format(
+                    name=attr.name,
+                    length=self.length,
+                    actual=len(value),
+                    value=value,
+                ),
+                attr,
+                self.length,
+                value)
+
 
     def __call__(self, inst, attr, value):
         """
         We use a callable class to be able to change the ``__repr__``.
         """
         if self.length.startswith('>'):
-            if not len(value) > int(self.length.strip('>')):
-                raise TypeError(
-                    "'{name}' must be longer than {length!r} (got {value!r} that is a "
-                    "{actual!r}).".format(
-                        name=attr.name,
-                        length=self.length.strip('>'),
-                        actual=value.__class__,
-                        value=value,
-                    ),
-                    attr,
-                    self.length,
-                    value)
+            return self._gt(inst, attr, value)
 
-        if self.length.startswith('<'):
-            if not len(value) < int(self.length.strip('<')):
-                raise TypeError(
-                    "length of '{name}' must be less than {length!r} (got {value!r} that is a "
-                    "{actual!r}).".format(
-                        name=attr.name,
-                        length=self.length.strip('<'),
-                        actual=len(value),
-                        value=value,
-                    ),
-                    attr,
-                    self.length,
-                    value)
+        elif self.length.startswith('<'):
+            return self._gt(inst, attr, value)
+
+        elif self.length.startswith('>='):
+            return self._gte(inst, attr, value)
+
+        elif self.length.startswith('<='):
+            return self._gte(inst, attr, value)
+        else:
+            return self._gte(inst, attr, value)
 
     def __repr__(self):
         return "<instance_of validator for length {length!r}>".format(
-            length=self.length
-        )
+            length=self.length)
 
 
 def has_length(length):
     return _LengthValidator(length)
 
 
+# this may not be useful - may be due to lack of understanding of attrs
+# seems useful to me, and gets things moving for now
 @attr.s(repr=False, slots=True, hash=True)
-class _MembersValidator(object):
+class _AllMembersValidator(object):
     members = attr.ib()
 
     def __call__(self, inst, attr, value):
@@ -75,5 +135,5 @@ class _MembersValidator(object):
         )
 
 
-def has_members(members):
-    return _MembersValidator(members)
+def all_members(membertest):
+    return _AllMembersValidator(membertest)
