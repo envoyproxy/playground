54eae2f349da22c335bfe46c5c2953d284faf102
Ryan Northey <ryan@synca.io>
2020-11-27 12:58:20 +0000

    tests
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -19,8 +19,8 @@ py_tests () {
     COMPOSE_FILE=./dev/docker-compose.yaml docker-compose build control
     COMPOSE_FILE=./dev/docker-compose.yaml docker-compose run \
 		--entrypoint /bin/sh \
-		control -c 'pytest --ignore-glob=*~ --flake8 --cov-append --cov=playground.control --cov-report=term --cov-report=xml:coverage/coverage.xml tests/*py'
+		control -c 'pytest --ignore-glob=*~ --flake8 --cov-append --cov=playground.control --cov=tests --cov-report=term --cov-report=xml:coverage/coverage.xml tests/*py'
 }
 
-# js_tests
+js_tests
 py_tests
