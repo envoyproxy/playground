60f6bd772d12162b5fb10c73c7ce34166d00c11c
Ryan Northey <ryan@synca.io>
2020-11-28 18:19:58 +0000

    tests/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tests/py/connectors/docker/client.py
+++ tests/py/connectors/docker/client.py
@@ -10,8 +10,8 @@ def test_docker_client(patch_playground):
 
     with _patch_events as m_events:
         with _patch_aiodocker as m_aiodocker:
-            _client = client.PlaygroundDockerClient()
-            assert _client.client == m_aiodocker.return_value
+            _connector = client.PlaygroundDockerClient()
+            assert _connector.docker == m_aiodocker.return_value
             assert (
                 list(m_events.call_args)
                 == [((m_aiodocker.return_value), ), {}])
