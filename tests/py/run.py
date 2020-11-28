02883750941321e9b78ed0634ace45d15a17b775
Ryan Northey <ryan@synca.io>
2020-11-28 16:30:59 +0000

    tests/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tests/py/run.py
+++ tests/py/run.py
@@ -4,9 +4,16 @@ from unittest.mock import patch
 from playground.control import run
 
 
-def test_run_main():
+def test_run_runner():
     with patch('playground.control.run.PlaygroundRunner') as m_runner:
         run.main()
         assert (
             list(m_runner.call_args)
             == [(run.ENDPOINTS, run.CORS_ALLOWED), {}])
+        assert m_runner.return_value.run.called
+
+
+def test_run_main(script_runner):
+    with patch('playground.control.run.main') as m_main:
+        script_runner.run("playground")
+        assert m_main.called
