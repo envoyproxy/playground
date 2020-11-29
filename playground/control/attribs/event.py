e68452d5df927119e8dd73a3065683cf51b8c869
Ryan Northey <ryan@synca.io>
2020-11-29 23:07:54 +0000

    playground/
    Signed-off-by: Ryan Northey <ryan@synca.io>

---
+++ playground/control/attribs/event.py
@@ -0,0 +1,24 @@
+
+import attr
+
+from playground.control.attribs.base import ValidatingAttribs
+
+
+@attr.s(kw_only=True)
+class ContainerEventAttribs(ValidatingAttribs):
+    id = attr.ib(type=str)
+    status = attr.ib(type=str)
+    action = attr.ib(type=str)
+    attributes = attr.ib(type=dict)
+
+
+@attr.s(kw_only=True)
+class ImageEventAttribs(ValidatingAttribs):
+    action = attr.ib(type=str)
+
+
+@attr.s(kw_only=True)
+class NetworkEventAttribs(ValidatingAttribs):
+    id = attr.ib(type=str)
+    action = attr.ib(type=str)
+    name = attr.ib(type=str)
