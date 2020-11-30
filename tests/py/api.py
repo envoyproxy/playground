8ff1077b59336be4838f0324a7b8e04b83875b69
Ryan Northey <ryan@synca.io>
2020-11-30 06:04:06 +0000

    tests/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tests/py/api.py
+++ tests/py/api.py
@@ -1,5 +1,5 @@
 
-from unittest.mock import AsyncMock, MagicMock
+from unittest.mock import AsyncMock, MagicMock, PropertyMock
 
 import rapidjson as json
 
@@ -59,18 +59,21 @@ async def test_api_clear(patch_playground):
     _api.connector = MagicMock()
     _api.connector.clear = AsyncMock()
     _patch_resp = patch_playground('api.listener.web.json_response')
+    _patch_request = patch_playground('api.listener.PlaygroundRequest')
     _request = DummyPlaygroundRequest()
 
     with _patch_resp as m_resp:
-        response = await _api.clear(_request)
-        assert response == m_resp.return_value
-        assert (
-            list(m_resp.call_args)
-            == [({'message': 'OK'},),
-                {'dumps': json.dumps}])
-        assert (
-            list(_api.connector.clear.call_args)
-            == [(), {}])
+        with _patch_request as m_request:
+            response = await _api.clear(_request)
+            assert response == m_resp.return_value
+            assert (
+                list(m_resp.call_args)
+                == [({'message': 'OK'},),
+                    {'dumps': json.dumps}])
+            assert (
+                list(_api.connector.clear.call_args)
+                == [(), {}])
+            assert not m_request.called
 
 
 @pytest.mark.asyncio
@@ -78,20 +81,24 @@ async def test_api_dump_resources(patch_playground):
     _api = DummyPlaygroundAPI()
     _api.connector = MagicMock()
     _api.connector.dump_resources = AsyncMock(return_value=MagicMock())
+    type(_api).service_types = PropertyMock()
     _patch_resp = patch_playground('api.listener.web.json_response')
+    _patch_request = patch_playground('api.listener.PlaygroundRequest')
     _request = DummyPlaygroundRequest()
 
     with _patch_resp as m_resp:
-        response = await _api.dump_resources(_request)
-        assert response == m_resp.return_value
-        assert (
-            list(_api.connector.dump_resources.call_args)
-            == [(), {}])
-        assert (
-            list(_api.connector.dump_resources.return_value.update.call_args)
-            == [({'meta': _api.metadata,
-                  'service_types': _api.service_types}, ), {}])
-        assert (
-            list(m_resp.call_args)
-            == [(_api.connector.dump_resources.return_value,),
-                {'dumps': json.dumps}])
+        with _patch_request as m_request:
+            response = await _api.dump_resources(_request)
+            assert response == m_resp.return_value
+            assert (
+                list(_api.connector.dump_resources.call_args)
+                == [(), {}])
+            assert (
+                list(_api.connector.dump_resources.return_value.update.call_args)
+                == [({'meta': _api.metadata,
+                      'service_types': _api.service_types}, ), {}])
+            assert (
+                list(m_resp.call_args)
+                == [(_api.connector.dump_resources.return_value,),
+                    {'dumps': json.dumps}])
+            assert not m_request.called
