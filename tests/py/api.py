a479771aabfe23d97e7a05d3e71c9f4a34107d12
Ryan Northey <ryan@synca.io>
2020-11-30 05:37:54 +0000

    tests: Add more python tests
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tests/py/api.py
+++ tests/py/api.py
@@ -1,13 +1,96 @@
 
-from playground.control import api
+from unittest.mock import AsyncMock, MagicMock
+
+import rapidjson as json
+
+import pytest
+
+from playground.control import api, request
+from playground.control.constants import (
+    MIN_NAME_LENGTH, MAX_NAME_LENGTH,
+    MIN_CONFIG_LENGTH, MAX_CONFIG_LENGTH,
+    MAX_NETWORK_CONNECTIONS)
+
+
+class DummyPlaygroundAPI(api.PlaygroundAPI):
+
+    def __init__(self):
+        pass
+
+
+class DummyPlaygroundRequest(request.PlaygroundRequest):
+
+    def __init__(self):
+        pass
 
 
 def test_api(patch_playground):
     _patch_docker = patch_playground('api.listener.PlaygroundDockerClient')
+    _patch_handler = patch_playground('api.listener.PlaygroundEventHandler')
 
     with _patch_docker as m_docker:
-        _api = api.PlaygroundAPI()
-        assert _api.connector == m_docker.return_value
+        with _patch_handler as m_handler:
+            _api = api.PlaygroundAPI()
+            assert _api.connector == m_docker.return_value
+            assert (
+                list(m_docker.call_args)
+                == [(), {}])
+            assert _api.handler == m_handler.return_value
+            assert (
+                list(m_handler.call_args)
+                == [(_api,), {}])
+
+
+def test_api_metadata():
+    _api = DummyPlaygroundAPI()
+    assert _api._envoy_image == "envoyproxy/envoy-dev:latest"
+    assert _api.metadata == dict(
+        version=_api._envoy_image,
+        max_network_connections=MAX_NETWORK_CONNECTIONS,
+        min_name_length=MIN_NAME_LENGTH,
+        max_name_length=MAX_NAME_LENGTH,
+        min_config_length=MIN_CONFIG_LENGTH,
+        max_config_length=MAX_CONFIG_LENGTH)
+
+
+@pytest.mark.asyncio
+async def test_api_clear(patch_playground):
+    _api = DummyPlaygroundAPI()
+    _api.connector = MagicMock()
+    _api.connector.clear = AsyncMock()
+    _patch_resp = patch_playground('api.listener.web.json_response')
+    _request = DummyPlaygroundRequest()
+
+    with _patch_resp as m_resp:
+        response = await _api.clear(_request)
+        assert response == m_resp.return_value
         assert (
-            list(m_docker.call_args)
+            list(m_resp.call_args)
+            == [({'message': 'OK'},),
+                {'dumps': json.dumps}])
+        assert (
+            list(_api.connector.clear.call_args)
             == [(), {}])
+
+
+@pytest.mark.asyncio
+async def test_api_dump_resources(patch_playground):
+    _api = DummyPlaygroundAPI()
+    _api.connector = MagicMock()
+    _api.connector.dump_resources = AsyncMock(return_value=MagicMock())
+    _patch_resp = patch_playground('api.listener.web.json_response')
+    _request = DummyPlaygroundRequest()
+
+    with _patch_resp as m_resp:
+        response = await _api.dump_resources(_request)
+        assert response == m_resp.return_value
+        assert (
+            list(_api.connector.dump_resources.call_args)
+            == [(), {}])
+        assert (
+            list(_api.connector.dump_resources.return_value.update.call_args)
+            == [({'meta': _api.metadata, 'service_types': _api.service_types}, ), {}])
+        assert (
+            list(m_resp.call_args)
+            == [(_api.connector.dump_resources.return_value,),
+                {'dumps': json.dumps}])
