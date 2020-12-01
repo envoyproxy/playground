aff26cb75aa5ba503e6526e3284a1aa0e605906c
Ryan Northey <ryan@synca.io>
2020-12-01 18:23:06 +0000

    tests/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tests/integration/test_playground.py
+++ tests/integration/test_playground.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 driver = webdriver.Remote(
-   command_executor='http://127.0.0.1:4444/wd/hub',
+    command_executor='http://127.0.0.1:4444/wd/hub',
     desired_capabilities=DesiredCapabilities.FIREFOX)
 
 driver.get("http://localhost:8000")
