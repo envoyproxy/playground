88ed4f1eddb50eae4d08122184a61b4071e6fac0
Ryan Northey <ryan@synca.io>
2020-12-01 15:35:14 +0000

    tests/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tests/integration/playground.py
+++ tests/integration/playground.py
@@ -1,5 +1,3 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
