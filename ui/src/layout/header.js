f4fb155fea0bf201a6bf6fc4f1432363e4f2e59f
Ryan Northey <ryan@synca.io>
2020-12-01 07:41:20 +0000

    header
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ui/src/layout/header.js
+++ ui/src/layout/header.js
@@ -3,20 +3,21 @@ import PropTypes from 'prop-types';
 import exact from 'prop-types-exact';
 import {connect} from 'react-redux';
 
+import {CustomInput, Col} from 'reactstrap';
+
+import {
+    PlaygroundForm, PlaygroundFormGroup,
+    PlaygroundFormGroupRow} from '../shared/forms';
 import {
     ActionClear,
     ActionLoad, ActionSave} from '../shared/actions';
-import {APIContext} from '../app/context';
+import {APIContext, ModalContext} from '../app/context';
 import EnvoyLogo from '../app/images/envoy.svg';
 import {updateUI} from '../app/store';
 
 
-class BaseHeader extends React.PureComponent {
-    static contextType = APIContext;
-    static propTypes = exact({
-        dispatch: PropTypes.func.isRequired,
-        version: PropTypes.string,
-    });
+class HeaderActions extends React.PureComponent {
+    static contextType = APIContext
 
     clear = async () => {
         await this.context.get('/clear');
@@ -29,11 +30,95 @@ class BaseHeader extends React.PureComponent {
 
     load = async () => {
         const {dispatch} = this.props;
-        dispatch(updateUI({modal: 'not-implemented'}));
+        dispatch(updateUI({modal: 'load'}));
     };
 
     render () {
-        const {version} = this.props;
+        return (
+            <>
+              <ActionClear
+                className="ml-2 mb-0 float-right mt-1 mr-5"
+                clear={this.clear} />
+              <ActionLoad load={this.load} />
+              <ActionSave save={this.save} />
+            </>);
+    }
+}
+
+export class BaseLoadPlaygroundForm extends React.PureComponent {
+
+    get messages () {
+        return [
+            "Select an example playground setup.",
+        ];
+    }
+
+    render () {
+        const {examples} = this.props;
+        const {playground} = examples;
+        return (
+            <PlaygroundForm messages={this.messages}>
+              <PlaygroundFormGroup>
+                <PlaygroundFormGroupRow
+                  label="examples"
+                  title="Playground examples">
+                  <Col sm={8}>
+                    <CustomInput
+                      type="select"
+                      id="default-level"
+                      onChange={this.onChange}
+                      name="default-level">
+                      <option>Select an example</option>);
+                      {Object.keys(playground).map((k, i) => {
+                          return (
+                              <option key={i}>{k}</option>);
+                          })}
+                    </CustomInput>
+                  </Col>
+                </PlaygroundFormGroupRow>
+              </PlaygroundFormGroup>
+            </PlaygroundForm>);
+    }
+
+}
+
+
+const mapExampleStateToProps = function(state) {
+    return {
+        examples: state.example.value,
+    };
+}
+
+const LoadPlaygroundForm = connect(mapExampleStateToProps)(BaseLoadPlaygroundForm);
+export {LoadPlaygroundForm}
+
+
+export class LoadPlaygroundModal extends React.PureComponent {
+
+    render () {
+        return (
+            <>
+              <LoadPlaygroundForm  />
+            </>);
+    }
+
+}
+
+class BaseHeader extends React.PureComponent {
+    static contextType = ModalContext;
+    static propTypes = exact({
+        dispatch: PropTypes.func.isRequired,
+        version: PropTypes.string,
+    });
+
+    componentDidMount () {
+        this.context['load'] = {
+            modal: LoadPlaygroundModal,
+            title: () => "Load playground"};
+    }
+
+    render () {
+        const {dispatch, version} = this.props;
         return (
             <header className="App-header bg-dark border-bottom border-dark">
               <img
@@ -42,18 +127,15 @@ class BaseHeader extends React.PureComponent {
                 width="28px"
                 className="ml-1 mr-2" />
 	      <span>Envoy playground ({version})</span>
-              <ActionClear
-                className="ml-2 mb-0 float-right mt-1 mr-5"
-                clear={this.clear} />
-              <ActionLoad load={this.load} />
-              <ActionSave save={this.save} />
+              <HeaderActions
+                dispatch={dispatch} />
             </header>);
     }
 }
 
 const mapStateToProps = function(state) {
     return {
-        version: state.meta.value.version
+        version: state.meta.value.version,
     };
 }
 
