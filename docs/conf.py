a0f8446d67dc8f51acd60da1279fc22d7b31a420
Ryan Northey <ryan@synca.io>
2020-12-02 16:39:29 +0000

    docs: Add markdown support
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- docs/conf.py
+++ docs/conf.py
@@ -10,34 +10,29 @@ import sys
 def setup(app):
     pass
 
+
 extensions = [
+    'recommonmark',
     'sphinxcontrib.httpdomain', 'sphinx.ext.extlinks', 'sphinx.ext.ifconfig', 'sphinx_tabs.tabs',
-    'sphinx_copybutton', 'sphinxext.rediraffe'
-]
-
-
+    'sphinx_copybutton', 'sphinxext.rediraffe']
 copybutton_prompt_text = r"\$ |PS>"
 copybutton_prompt_is_regexp = True
-
-source_suffix = '.rst'
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown'}
 master_doc = 'index'
-
 project = u'Envoy playground'
 copyright = u'2020-{}, Envoy playground project authors'.format(datetime.now().year)
 author = u'Envoy playground project authors'
-
 exclude_patterns = [
     '_build',
     '_venv',
     'Thumbs.db',
-    '.DS_Store',
-]
-
+    '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     'logo_only': True,
-    'includehidden': False,
-}
+    'includehidden': False}
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_logo = '_static/img/envoy.svg'
 html_favicon = 'favicon.ico'
