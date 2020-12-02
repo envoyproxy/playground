fad63e7ffe0f16bc11e7bf9e1d4df0a602e0aeed
Ryan Northey <ryan@synca.io>
2020-12-02 11:37:52 +0000

    playground/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- playground/control/api/listener.py
+++ playground/control/api/listener.py
@@ -113,7 +113,7 @@ class PlaygroundAPI(object):
     async def publish(
             self,
             event: dict) -> None:
-        print("PUBLISH", self._sockets, event)
+        # print("PUBLISH", self._sockets, event)
         for socket in self._sockets:
             await socket.send_json(event, dumps=json.dumps)
 
