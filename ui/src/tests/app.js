ff75ef2fcca0b8c103248e1222f9999672a81f8a
Ryan Northey <ryan@synca.io>
2020-12-05 10:06:59 +0000

    ui/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ui/src/tests/app.js
+++ ui/src/tests/app.js
@@ -1,58 +1,18 @@
 
-import {Playground} from '../app';
-import PlaygroundAPI from '../app/api';
-import PlaygroundSocket from '../app/socket';
+import {Provider} from 'react-redux';
+
+import {shallow} from "enzyme";
+
+import {
+    PlaygroundApp, PlaygroundContext,
+    playground, store} from '../app';
 
 
-jest.mock('../app/api');
-jest.mock('../app/socket');
-
-let init;
-
-
-class DummyPlayground extends Playground {
-    init = init;
-}
-
-
-beforeEach(() => {
-    init = Playground.prototype.init;
-    Playground.prototype.init = jest.fn();
-});
-
-
-afterEach(() => {
-    Playground.prototype.init = init;
-});
-
-
-test('playground constructor', () => {
-    const store = {};
-    const playground = new Playground(store, 'API', 'SOCKET');
-    expect(playground.store).toEqual(store);
-    expect(playground.apiAddress).toEqual('API');
-    expect(playground.socketAddress).toEqual('SOCKET');
-    expect(playground.init.mock.calls).toEqual([[]]);
-});
-
-
-test('playground init', () => {
-    const playground = new DummyPlayground();
-    playground.apiAddress = 'API';
-    playground.socketAddress = 'SOCKET';
-    playground.init();
-    expect(PlaygroundAPI.mock.calls).toEqual([['API']]);
-    expect(PlaygroundSocket.mock.calls).toEqual([[playground, 'SOCKET']]);
-    expect(playground.modals).toEqual({});
-    expect(playground.toast).toEqual({});
-});
-
-
-test('playground load', async () => {
-    const playground = new Playground();
-    playground.loadData = jest.fn(async () => {});
-    playground.api = {get: jest.fn(async () => 'DATA')};
-    await playground.load();
-    expect(playground.api.get.mock.calls).toEqual([['/resources']]);
-    expect(playground.loadData.mock.calls).toEqual([['DATA']]);
+test('PlaygroundApp render', () => {
+    const app = shallow(<PlaygroundApp />);
+    expect(app.text()).toBe('<Provider />');
+    const provider = app.find(Provider);
+    expect(provider.props().store).toEqual(store);
+    const context = provider.find(PlaygroundContext.Provider);
+    expect(context.props.value).toEqual(playground);
 });
