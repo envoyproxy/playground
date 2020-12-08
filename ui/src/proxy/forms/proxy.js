3bd09f131587cb7b3e93375bc8fd8c4d58801484
Ryan Northey <ryan@synca.io>
2020-12-08 04:13:36 +0000

    ui/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ui/src/proxy/forms/proxy.js
+++ ui/src/proxy/forms/proxy.js
@@ -6,10 +6,10 @@ import {connect} from 'react-redux';
 import {Col} from 'reactstrap';
 
 import {updateForm} from '../../app/store';
-import {PlaygroundEditor} from '../../shared/editor';
 import {
     PlaygroundForm, PlaygroundFormGroup,
     PlaygroundFormGroupRow, PlaygroundNameInput} from '../../shared/forms';
+import {ProxyConfigForm} from './config';
 
 import Yaml from 'js-yaml';
 
@@ -22,64 +22,6 @@ const code =
     ...
 `;
 
-// VALIDATION REQUIRED
-//  - code:
-//      - is set
-//      - valid yaml
-//      - not too long, and not too short
-//      - ideally valid envoy config
-
-
-export class ProxyConfigForm extends React.PureComponent {
-    static propTypes = exact({
-        dispatch: PropTypes.func.isRequired,
-        onChange: PropTypes.func.isRequired,
-        configuration: PropTypes.string.isRequired,
-        errors: PropTypes.object.isRequired,
-        examples: PropTypes.object.isRequired,
-    });
-
-    clearConfig = () => {
-        const {dispatch} = this.props;
-        dispatch(updateForm({configuration: ''}));
-    }
-
-    onExampleSelect = async (evt) => {
-        const {dispatch, errors: _errors, examples} = this.props;
-        const {envoy} = examples;
-        const errors = {..._errors};
-        // todo: implement overwrite button
-        // if (configuration !== code)
-        if (envoy[evt.target.value]) {
-            const response = await fetch(envoy[evt.target.value].path);
-            const text = await response.text();
-            delete errors.configuration;
-            await dispatch(updateForm({configuration: text, valid: true, errors}));
-        } else {
-            await dispatch(updateForm({configuration: code, valid: false}));
-        }
-    }
-
-    render ()  {
-        const {configuration, examples, errors, onChange} = this.props;
-        const {envoy={}} = examples;
-        const {configuration: configErrors=[]} =  errors;
-        return (
-            <PlaygroundFormGroup>
-              <PlaygroundEditor
-                title="Configuration*"
-                name="configuration"
-                content={configuration}
-                format="yaml"
-                examples={envoy}
-                clearConfig={this.clearConfig}
-                onChange={onChange}
-                onExampleSelect={this.onExampleSelect}
-                errors={configErrors}
-              />
-            </PlaygroundFormGroup>);
-    }
-}
 
 export class BaseProxyForm extends React.PureComponent {
     static propTypes = exact({
