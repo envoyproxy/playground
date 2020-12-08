d470c7c91756ac673477b733326778986cd799fa
Ryan Northey <ryan@synca.io>
2020-12-08 08:17:30 +0000

    ui/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ui/src/shared/logs.js
+++ ui/src/shared/logs.js
@@ -3,6 +3,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import exact from 'prop-types-exact';
 
+import {LazyLog} from 'react-lazylog';
+
 import {PlaygroundEditor} from './editor';
 
 
@@ -13,15 +15,37 @@ export class PlaygroundFailLogs extends React.PureComponent {
 
     render () {
         const {logs} = this.props;
+        let i = 1;
+        let highlightStart;
+        let highlightStop;
+        let highlight;
+        for (const line of logs) {
+            if (line.toLowerCase().indexOf('error') !== -1 || line.toLowerCase().indexOf('invalid') !== -1) {
+                if (!highlightStart) {
+                    highlightStart = i;
+                } else {
+                    highlightStop = i;
+                }
+            } else {
+                if (highlightStart) {
+                    if (!highlightStop) {
+                        highlightStop = i;
+                    }
+                }
+            }
+            i += 1;
+        }
+        if (highlightStart) {
+            highlight = [highlightStart,  highlightStop || logs.length + 1];
+        }
         return (
-            <>
-              <PlaygroundEditor
-                title="Logs"
-                name="logs"
-                content={logs.join('')}
-                format="yaml"
-                onChange={code => code}
-              />
-            </>);
+            <div style={{ height: '50vh', width: '100%' }}>
+              <LazyLog
+                enableSearch
+                extraLines={2}
+                scrollToLine={highlightStop + 2 || logs.length + 1}
+                highlight={highlight}
+                text={logs.join('')} />
+            </div>);
     }
 }
