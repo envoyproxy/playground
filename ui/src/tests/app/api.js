060cebbe3f55fd15d04a7bc52254c560b621e3ca
Ryan Northey <ryan@synca.io>
2020-12-10 16:27:53 +0000

    ui/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ui/src/tests/app/api.js
+++ ui/src/tests/app/api.js
@@ -32,6 +32,24 @@ test('api get', async () => {
 });
 
 
+test('api post', async () => {
+    const _fetch = global.fetch;
+    const response = {json: jest.fn(async () => 'JSON')};
+    global.fetch = jest.fn(async () => response);
+    const playground = {PG: true};
+    const api = new PlaygroundAPI(playground, 'ADDRESS');
+    api._getAddress = jest.fn(() => 'FQADDRESS');
+    api._getPostPayload = jest.fn(() => 'POSTDATA');
+    const result = await api.post('/PATH', 'DATA');
+    expect(result).toEqual('JSON');
+    expect(global.fetch.mock.calls).toEqual([['FQADDRESS', 'POSTDATA']]);
+    expect(response.json.mock.calls).toEqual([[]]);
+    expect(api._getAddress.mock.calls).toEqual([['/PATH']]);
+    expect(api._getPostPayload.mock.calls).toEqual([['DATA']]);
+    global.fetch = _fetch;
+});
+
+
 test('api errors', async () => {
     const playground = {store: {dispatch: jest.fn(async () => {})}};
     updateUI.mockImplementation(() => 'UPDATEDUI');
