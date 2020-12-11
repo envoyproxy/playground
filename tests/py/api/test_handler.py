642eb41b40fb089bfe1eee40e0829309edb5f436
Ryan Northey <ryan@synca.io>
2020-12-11 10:48:52 +0000

    tests/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tests/py/api/test_handler.py
+++ tests/py/api/test_handler.py
@@ -11,7 +11,10 @@ from playground.control import api, event
 class DummyPlaygroundAPI(api.PlaygroundAPI):
 
     def __init__(self):
-        self.connector = MagicMock()
+        self._publisher = AsyncMock()
+
+    async def publish_network(self, event):
+        await self._publisher(event)
 
 
 class DummyEvent(event.PlaygroundEvent):
@@ -24,6 +27,10 @@ class DummyEvent(event.PlaygroundEvent):
 class DummyData(object):
     action = attr.ib(type=str, default='')
     id = attr.ib(type=str, default='')
+    name = attr.ib(type=str, default='')
+    containers = attr.ib(type=list, default=[])
+    proxy = attr.ib(type=str, default='')
+    service = attr.ib(type=str, default='')
 
 
 def test_api_handler(patch_playground):
@@ -125,6 +132,74 @@ async def test_api_network_handler_destroy(patch_playground):
         assert (
             list(m_publish.call_args)
             == [(event, {'id': 'XXXXXXXXXX'}), {}])
+
+
+@pytest.mark.asyncio
+async def test_api_network_handler_dunder_publish(patch_playground):
+    _dummy_api = DummyPlaygroundAPI()
+    handler = api.handler.PlaygroundNetworkEventHandler(_dummy_api)
+    event = DummyEvent()
+    event._data = DummyData(id='X' * 20, action='ACTION', name='NAME')
+    await handler._publish(event, dict(FOO='BAR', BAZ='OTHER', name='NONAME'))
+    assert (
+        list(_dummy_api._publisher.call_args)
+        == [({'id': 'XXXXXXXXXX',
+              'action': 'ACTION',
+              'name': 'NONAME',
+              'FOO': 'BAR',
+              'BAZ': 'OTHER'},), {}])
+
+
+@pytest.mark.asyncio
+async def test_api_network_connection(patch_playground):
+    _dummy_api = DummyPlaygroundAPI()
+    handler = api.handler.PlaygroundNetworkEventHandler(_dummy_api)
+    event = DummyEvent()
+    event._data = DummyData(id='X' * 20, action='ACTION', name='NAME')
+    _patch_publish = patch_playground(
+        'api.handler.network.PlaygroundNetworkEventHandler._publish',
+        new_callable=AsyncMock)
+
+    with _patch_publish as m_publish:
+        await handler._connection(event)
+        assert (
+            list(m_publish.call_args)
+            == [(event,
+                 {'networks': {
+                     'NAME': {
+                         'name': 'NAME',
+                         'id': 'XXXXXXXXXX',
+                         'containers': []}}}), {}])
+
+
+@pytest.mark.asyncio
+async def test_api_network_connection_args(patch_playground):
+    _dummy_api = DummyPlaygroundAPI()
+    handler = api.handler.PlaygroundNetworkEventHandler(_dummy_api)
+    event = DummyEvent()
+    event._data = DummyData(
+        id='X' * 20,
+        action='ACTION',
+        name='NAME',
+        containers=['CONTAINER1', 'CONTAINER2'],
+        proxy='PROXY1',
+        service='SERVICE1')
+    _patch_publish = patch_playground(
+        'api.handler.network.PlaygroundNetworkEventHandler._publish',
+        new_callable=AsyncMock)
+
+    with _patch_publish as m_publish:
+        await handler._connection(event)
+        assert (
+            list(m_publish.call_args)
+             == [(event,
+                  {'networks': {
+                      'NAME': {
+                          'name': 'NAME',
+                          'id': 'XXXXXXXXXX',
+                          'containers': ['CONTAINER1', 'CONTAINER2']}},
+                   'proxy': 'PROXY1',
+                   'service': 'SERVICE1'}), {}])
 
 
 @pytest.mark.parametrize(
