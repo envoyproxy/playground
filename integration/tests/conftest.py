e1be7ee34236eb20221403cbbc2952cb79c92461
Ryan Northey <ryan@synca.io>
2020-12-12 19:56:11 +0000

    integration/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- integration/tests/conftest.py
+++ integration/tests/conftest.py
@@ -44,7 +44,6 @@ def str2bool(v):
 
 
 def pytest_configure(config):
-    # register an additional marker
     config.addinivalue_line(
         "markers", "screenshots: user journeys that create screenshots")
 
