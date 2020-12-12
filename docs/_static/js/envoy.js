84b67567e3e2246977ef26c49749cd0387d7ede9
Ryan Northey <ryan@synca.io>
2020-12-12 09:57:07 +0000

    docs/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- docs/_static/js/envoy.js
+++ docs/_static/js/envoy.js
@@ -5,10 +5,9 @@
             if (!$('#img01').length) {
                 $(document.body).append(
                     '<div class="modal" id="playground-modal"><span class="modal-close">&times;</span><img class="modal-content" id="img01"></div>');
-                $('.modal-close').onclick = function() {
-                    console.log('click closed...');
+                $('.modal-close').click(function () {
                     $('#playground-modal').hide();
-                };
+                });
             }
             $('#playground-modal').show();
             $('#img01').attr('src', $('img', this).attr('src'));
