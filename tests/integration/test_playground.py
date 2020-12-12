c3364b2945d9dbef6edafc18c1211b05779a4e85
Ryan Northey <ryan@synca.io>
2020-12-12 11:43:08 +0000

    docs
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tests/integration/test_playground.py
+++ tests/integration/test_playground.py
@@ -20,19 +20,21 @@ def test_open_proxy_modal(selenium):
     assert (
         name_input.get_attribute('placeholder')
         == 'Enter proxy name')
-    time.sleep(.1)
+    time.sleep(.3)
     selenium.get_screenshot_as_file('/tmp/tests/proxy.create.name.png')
 
-    # enter name of proxy
+    # add configuration
     name_input.send_keys('proxy0')
-    time.sleep(.1)
+    select = selenium.find_element_by_css_selector('.tab-pane.active form select#example')
+    select.find_element_by_css_selector('[value="Service: Python (asyncio)"]').click()
+    time.sleep(.3)
     selenium.get_screenshot_as_file('/tmp/tests/proxy.create.configuration.png')
 
     # click the ports dialog
     selenium.find_element_by_link_text('Ports').click()
-    time.sleep(.1)
+    time.sleep(.3)
 
     selenium.find_element_by_css_selector('.tab-pane.active form button').click()
 
-    time.sleep(.1)
+    time.sleep(.3)
     selenium.get_screenshot_as_file('/tmp/tests/proxy.create.ports.png')
