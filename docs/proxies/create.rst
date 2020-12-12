afc43c841178707cffef0c930ce7fb0dcf1e6fab
Ryan Northey <ryan@synca.io>
2020-12-12 11:01:52 +0000

    screenshots
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- docs/proxies/create.rst
+++ docs/proxies/create.rst
@@ -42,7 +42,7 @@ This should open the proxy create dialogue.
 Enter the proxy name and optionally select a version
 ----------------------------------------------------
 
-..  figure:: ../_static/screenshots/proxy-create.png
+..  figure:: ../_static/screenshots/proxy.create.png
     :figclass: screenshot with-shadow
     :figwidth: 40%
     :align: right
@@ -67,7 +67,7 @@ You can ensure you are using the most recently published image by selecting the
 Add an Envoy configuration
 --------------------------
 
-..  figure:: ../_static/screenshots/proxy-create-name.png
+..  figure:: ../_static/screenshots/proxy.create.name.png
     :figclass: screenshot with-shadow
     :figwidth: 40%
     :align: right
@@ -84,6 +84,11 @@ You can also add your own, or edit any of the examples to suit your needs.
 
 Add port mappings (optional)
 ----------------------------
+
+..  figure:: ../_static/screenshots/proxy.create.ports.png
+    :figclass: screenshot with-shadow
+    :figwidth: 40%
+    :align: right
 
 If you wish the proxy to be addressable from outside of the playground (eg. to localhost),
 you need to provide a port mapping.
--- tests/integration/test_playground.py
+++ tests/integration/test_playground.py
@@ -12,21 +12,32 @@ def test_title(selenium):
 def test_open_proxy_modal(selenium):
     selenium.get("http://localhost:8000")
 
+    time.sleep(1)
+
     # open the proxy modal
     selenium.find_elements_by_name('Proxies')[0].click()
     name_input = selenium.find_elements_by_id('name')[0]
     assert (
         name_input.get_attribute('placeholder')
         == 'Enter proxy name')
-    time.sleep(1)
-    selenium.get_screenshot_as_file('/tmp/tests/proxy-create.png')
+    time.sleep(.1)
+    selenium.get_screenshot_as_file('/tmp/tests/proxy.create.open.png')
 
     # enter name of proxy
     name_input.send_keys('proxy0')
-    time.sleep(1)
-    selenium.get_screenshot_as_file('/tmp/tests/proxy-create-name.png')
+    time.sleep(.1)
+    selenium.get_screenshot_as_file('/tmp/tests/proxy.create.name.png')
 
     # select an example configuration
     name_input.send_keys('proxy0')
-    time.sleep(1)
-    selenium.get_screenshot_as_file('/tmp/tests/proxy-create-name.png')
+    time.sleep(.1)
+    selenium.get_screenshot_as_file('/tmp/tests/proxy.create.name.png')
+
+    # click the ports dialog
+    selenium.find_element_by_link_text('Ports').click()
+    time.sleep(.1)
+
+    selenium.find_element_by_css_selector('.tab-pane.active form button').click()
+
+    time.sleep(.1)
+    selenium.get_screenshot_as_file('/tmp/tests/proxy.create.ports.png')
