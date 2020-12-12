da3f244f92ceb1cbf1cc72b834774f29e02d1e2e
Ryan Northey <ryan@synca.io>
2020-12-12 11:16:23 +0000

    tests/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tests/integration/test_playground.py
+++ tests/integration/test_playground.py
@@ -5,8 +5,6 @@ import time
 def test_title(selenium):
     selenium.get("http://localhost:8000")
     assert "Envoy" in selenium.title
-    time.sleep(1)
-    selenium.get_screenshot_as_file('/tmp/tests/home.png')
 
 
 def test_open_proxy_modal(selenium):
@@ -21,17 +19,12 @@ def test_open_proxy_modal(selenium):
         name_input.get_attribute('placeholder')
         == 'Enter proxy name')
     time.sleep(.1)
-    selenium.get_screenshot_as_file('/tmp/tests/proxy.create.open.png')
+    selenium.get_screenshot_as_file('/tmp/tests/proxy.create.name.png')
 
     # enter name of proxy
     name_input.send_keys('proxy0')
     time.sleep(.1)
-    selenium.get_screenshot_as_file('/tmp/tests/proxy.create.name.png')
-
-    # select an example configuration
-    name_input.send_keys('proxy0')
-    time.sleep(.1)
-    selenium.get_screenshot_as_file('/tmp/tests/proxy.create.name.png')
+    selenium.get_screenshot_as_file('/tmp/tests/proxy.create.configuration.png')
 
     # click the ports dialog
     selenium.find_element_by_link_text('Ports').click()
