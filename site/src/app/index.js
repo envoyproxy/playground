bee61a6c31519306d2e2ce3d4a990dd407a98a70
Ryan Northey <ryan@synca.io>
2020-12-13 08:16:38 +0000

    site: Add navbar with link to docs
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- site/src/app/index.js
+++ site/src/app/index.js
@@ -1,7 +1,11 @@
+
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+export default class PlaygroundSiteApp extends React.PureComponent {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,5 +14,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
