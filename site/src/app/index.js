e8f58f53fcf4081b030adf9ea543ea4f6fa087bd
Ryan Northey <ryan@synca.io>
2020-12-13 08:30:52 +0000

    site/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- site/src/app/index.js
+++ site/src/app/index.js
@@ -1,8 +1,8 @@
 
 import React from 'react';
 
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/logo.svg';
+import './css/playground-site.css';
 
 
 export default class PlaygroundSiteApp extends React.PureComponent {
