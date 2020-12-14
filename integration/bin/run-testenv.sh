402df7fc40460630adbb20e0837dfb8ea9ba380e
Ryan Northey <ryan@synca.io>
2020-12-14 22:37:23 +0000

    integration/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- integration/bin/run-testenv.sh
+++ integration/bin/run-testenv.sh
@@ -12,6 +12,8 @@ docker run --rm \
        -v /code/tests:/tests \
        -v /code/pytest.ini:/pytest.ini \
        -v /code/artifacts:/artifacts \
+       -v /var/run/docker.sock:/var/run/docker.sock \
+       --privileged \
        --entrypoint /entrypoint.sh \
        python:3.8-slim "$@"
 echo
