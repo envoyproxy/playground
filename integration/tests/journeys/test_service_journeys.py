545faf096592ba1721770582e8502f4b6a02a13a
Ryan Northey <ryan@synca.io>
2020-12-15 04:38:00 +0000

    integration/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- integration/tests/journeys/test_service_journeys.py
+++ integration/tests/journeys/test_service_journeys.py
@@ -1,16 +1,17 @@
 
-import time
+import asyncio
 
 import pytest
 
 
 @pytest.mark.screenshots
-def test_journey_service_create(playground):
-    playground.snap('service.create.open')
+@pytest.mark.asyncio
+async def test_journey_service_create(playground):
+    await playground.snap2('service.create.open')
 
     # open the proxy modal
     playground.web.find_elements_by_name('Services')[0].click()
-    time.sleep(1)
+    await asyncio.sleep(1)
     name_input = playground.web.find_elements_by_id(
         'envoy.playground.name')[0]
     assert (
@@ -19,18 +20,18 @@ def test_journey_service_create(playground):
 
     # add first 2 keys of name
     name_input.send_keys('echo')
-    playground.snap('service.create.name')
+    await playground.snap2('service.create.name')
 
     # select a service type
     select = playground.web.find_element_by_css_selector(
         '.tab-pane.active form select#service_type')
     select.find_element_by_css_selector(
         '[value="http-echo"]').click()
-    playground.snap('service.create.configuration', .3)
+    await playground.snap2('service.create.configuration', .3)
 
     # add an environment var
     playground.web.find_element_by_link_text('Environment').click()
-    time.sleep(.3)
+    await asyncio.sleep(.3)
 
     key = playground.web.find_element_by_id('key')
     key.send_keys('MYVAR')
@@ -39,23 +40,23 @@ def test_journey_service_create(playground):
 
     playground.web.find_element_by_css_selector(
         '.tab-pane.active form button').click()
-    playground.snap('service.create.env', .3)
+    await playground.snap2('service.create.env', .3)
 
     # view ports
     playground.web.find_element_by_link_text('Ports').click()
-    time.sleep(.3)
-    playground.snap('service.create.ports', .3)
+    await asyncio.sleep(.3)
+    await playground.snap2('service.create.ports', .3)
 
     # view readme
     playground.web.find_element_by_link_text('README').click()
-    time.sleep(.3)
-    playground.snap('service.create.readme', .3)
+    await asyncio.sleep(.3)
+    await playground.snap2('service.create.readme', .3)
 
     # click to start
     playground.web.find_element_by_css_selector(
         '.modal-footer .btn.btn-primary').click()
 
-    playground.snap('service.create.starting', .3)
+    await playground.snap2('service.create.starting', .3)
 
     # wait for started
-    playground.snap('service.create.started', 60)
+    await playground.snap2('service.create.started', 60)
