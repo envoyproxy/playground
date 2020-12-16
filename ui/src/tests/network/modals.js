46f71b84bc2006f9ef0258dce549fe5b9b845ac8
Ryan Northey <ryan@synca.io>
2020-12-16 18:55:52 +0000

    ui/
    Signed-off-by: Ryan Northey <ryan@synca.io>

---
+++ ui/src/tests/network/modals.js
@@ -0,0 +1,66 @@
+
+import React from 'react';
+
+import {shallow} from "enzyme";
+
+import {BaseNetworkFormModal} from '../../network/modals';
+import {PlaygroundFormModal} from '../../shared/modal';
+
+
+test('NetworkFormModal render', () => {
+    const dispatch = jest.fn(async () => {});
+    const onUpdate = jest.fn(async () => {});
+    const proxies = {
+        PROXY1: 'PROXYFOO',
+        PROXY2: 'PROXYBAR'};
+    const services = {
+        SERVICE1: 'SERVICEFOO',
+        SERVICE2: 'SERVICEBAR'};
+    const form = {};
+    const modal = shallow(
+        <BaseNetworkFormModal
+          form={form}
+          dispatch={dispatch}
+          proxies={proxies}
+          services={services}
+          onUpdate={onUpdate}
+        />);
+    expect(modal.text()).toEqual('');
+    const formModal = modal.find(PlaygroundFormModal);
+    expect(formModal.props()).toEqual({
+        "icon": "cloud.svg",
+        "messages": modal.instance().activityMessages,
+        success: 'create',
+        successTimeout: 1000,
+        tabs: modal.instance().tabs});
+});
+
+
+test('NetworkFormModal activityMessages', () => {
+    const dispatch = jest.fn(async () => {});
+    const onUpdate = jest.fn(async () => {});
+    const proxies = {
+        PROXY1: 'PROXYFOO',
+        PROXY2: 'PROXYBAR'};
+    const services = {
+        SERVICE1: 'SERVICEFOO',
+        SERVICE2: 'SERVICEBAR'};
+    const form = {name: 'SERVICENAME'};
+    const modal = shallow(
+        <BaseNetworkFormModal
+          form={form}
+          dispatch={dispatch}
+          proxies={proxies}
+          services={services}
+          onUpdate={onUpdate}
+        />);
+    expect(Object.keys(modal.instance().activityMessages)).toEqual(['default', 'success']);
+    expect(modal.instance().activityMessages.default[0]).toEqual([30, 100]);
+    expect(modal.instance().activityMessages.default[1].type).toEqual('span');
+    expect(modal.instance().activityMessages.default[1].props).toEqual(
+        {"children": ["Creating network (", "SERVICENAME", ")..."]});
+    expect(modal.instance().activityMessages.success[0]).toEqual([100, 100]);
+    expect(modal.instance().activityMessages.success[1].type).toEqual('span');
+    expect(modal.instance().activityMessages.success[1].props).toEqual(
+        {"children": ["Network created (", "SERVICENAME", ")..."]});
+});
