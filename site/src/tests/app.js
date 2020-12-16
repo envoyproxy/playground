526d231b2eebfaa19bbb27ec4ad9e366d57ae914
Ryan Northey <ryan@synca.io>
2020-12-16 15:07:26 +0000

    site/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- site/src/tests/app.js
+++ site/src/tests/app.js
@@ -1,5 +1,5 @@
-import {render} from '@testing-library/react';
-import PlaygroundSiteApp from '../app';
+// import {render} from '@testing-library/react';
+// import PlaygroundSiteApp from '../app';
 
 
 test('renders learn react link', () => {
