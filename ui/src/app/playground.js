b505930a031c46da7d67617c05035cd245b45b22
Ryan Northey <ryan@synca.io>
2020-12-17 11:32:53 +0000

    ui/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ui/src/app/playground.js
+++ ui/src/app/playground.js
@@ -8,50 +8,7 @@ import {
     updateServiceTypes, updateCloud, updateEdges, updateExamples,
 } from "./store";
 import {CloudItem} from './cloud';
-
-
-export class NameValidator {
-
-    constructor (store) {
-        this.store = store;
-    }
-
-    validate = (name, taken) => {
-        const state = this.store.getState();
-        const {value: meta} = state.meta;
-
-        const {max_length, min_length} = meta;
-        let valid = true;
-        const _errors = [];
-
-        if (name.length < parseInt(min_length)) {
-            valid = false;
-        }
-        if (name.length > parseInt(max_length)) {
-            valid = false;
-            _errors.push('Name too long, maximum ' + max_length + ' chars.');
-        }
-        for (const forbidden of ['..', '--', '__']) {
-            if (name.indexOf(forbidden) !== -1) {
-                valid = false;
-                _errors.push('Name cannot contain \'' + forbidden + '\'');
-            }
-        }
-        if (name.length > 0 && !name.match(/[a-z]+[a-z0-9.\-_]*$/)) {
-            valid = false;
-            _errors.push('Name contains forbidden characters');
-        }
-        if ((taken || []).indexOf(name) !== -1) {
-            valid = false;
-            _errors.push('Name exists already');
-        }
-        const errors = {};
-        if (_errors.length > 0) {
-            errors.name = _errors;
-        }
-        return {valid, errors};
-    }
-}
+import {NameValidator} from './validators';
 
 
 export default class Playground {
