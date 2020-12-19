39435ae56598173d2db4eb45b23892a3ad27b1b6
Ryan Northey <ryan@synca.io>
2020-12-19 18:46:09 +0000

    integration/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- integration/bin/remove-playground-images.sh
+++ integration/bin/remove-playground-images.sh
@@ -7,4 +7,4 @@ docker rmi -f redis:latest || :
 docker rmi -f envoyproxy/envoy-dev-playground:latest || :
 docker rmi -f envoyproxy/envoy-dev:latest || :
 docker rmi -f busybox:latest || :
-docker rmi -f mendhak/http-https-echo || :
+docker rmi -f phlax/aioecho || :
