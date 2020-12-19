ca661f7adcc2c3502f63e630c96e87e31aa9309a
phlax <phlax@users.noreply.github.com>
2020-12-19 05:02:31 +0000

    integration: Improve wait on dom queries (#335)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- integration/tests/journeys/test_network_journeys.py
+++ integration/tests/journeys/test_network_journeys.py
@@ -7,34 +7,30 @@ import pytest
 @pytest.mark.screenshots
 @pytest.mark.asyncio
 async def test_journey_network_create(playground):
-    await playground.snap('network.create.open')
+    await playground.snap('network.create.open', .3)
 
     # open the network modal
-    add_network_button = await playground.query('*[name="Networks"]')
+    add_network_button = await playground.query('*[name="Networks"]', 1)
     await add_network_button.click()
-    await asyncio.sleep(1)
 
     # find the name input
-    name_input = await playground.query('input[id="envoy.playground.name"]')
+    name_input = await playground.query('input[id="envoy.playground.name"]', 1)
     assert (
         await name_input.command('GET', '/attribute/placeholder')
         == 'Enter network name')
 
     # input the network name
     await playground.enter(name_input, 'net0')
-    await asyncio.sleep(1)
-    await playground.snap('network.create.name')
+    await playground.snap('network.create.name', .7)
 
     # submit the form
     submit = await playground.query('.modal-footer .btn.btn-primary')
     await submit.click()
-    await asyncio.sleep(.1)
     await playground.move('network:net0', 230, 230)
-    await playground.snap('network.create.starting')
-    await asyncio.sleep(1)
-    await playground.snap('network.create.started')
+    await playground.snap('network.create.starting', .2)
+    await playground.snap('network.create.started', 1)
     link = await playground.query(
-        '.App-left .accordion-item .card-header .col-sm-8')
+        '.App-left .accordion-item .card-header .col-sm-8', 5)
     assert (
         await link.text()
         == 'net0')
