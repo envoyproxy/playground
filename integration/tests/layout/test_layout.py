7202aa7fab6abfc6e104251d96d4355b5c7b36e9
Ryan Northey <ryan@synca.io>
2020-12-21 09:11:31 +0000

    integration/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- integration/tests/layout/test_layout.py
+++ integration/tests/layout/test_layout.py
@@ -4,6 +4,7 @@ import pytest
 
 @pytest.mark.asyncio
 async def test_title(playground):
+    title = await playground.query('title', 2)
     assert (
-        await playground.web.get_title()
+        await title.text()
         == "Envoy playground")
