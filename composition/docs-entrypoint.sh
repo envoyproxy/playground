fc8c3336882118f5597856db47330f67fb2b9fa7
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-29 10:12:16 +0800

    Delete composition directory

--- composition/docs-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-pip install -q .[docs]
-
-rm -rf build/docs
-rm -rf build/site/docs
-
-exec "$@"
--- composition/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-pip install -q -e '.[test]'
-pip install -q -e .
-
-exec "$@"
